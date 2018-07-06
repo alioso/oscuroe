20dbbcf0cf9af35d93113bd4491c0ab9726ff864
Alban Bailly <abailly@linode.com>
2018-07-06 17:45:14 -0400

    adding events

---
+++ _generated/assets/hbs_templates/backtonav.js
@@ -0,0 +1,3 @@
+define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<ul class=\"back-to-nav\">\n  <li><a href=\"/\">Home</a></li>\n  <li><a href=\"/\">Contact</a></li>\n</ul>";
+},"useData":true}); });
