665623869e507d56c10fddba622c6a42e68d6bab
Alban Bailly <abailly@linode.com>
2018-07-07 23:49:56 -0400

    wysiwyg

--- cms/index.js
+++ cms/index.js
@@ -2,11 +2,12 @@
 	about: {
 		header: 'About The Quintet',
 		headline: 'A Quote',
-		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-		$content_type: 'textarea'
+		$content_type: 'quill',
+		content: '<p><span class="ql-size-large">Founded in 2006 in Philadelphia, Oscuro Quintet is hailed as “a modern super group… one of the most original and acclaimed tango ensembles on the East Coast.”- WHYY TV On Tour. Inspired by Astor Piazzolla’s signature amalgam of tango, jazz, and classical music, Oscuro Quintet embarked on a quest to present tango of past and now and create new music for this unique instrumentation.</span></p><p><span class="ql-size-large">The members of Oscuro Quintet come from classical, jazz, and tango background and have studied in the US, South America, Asia, and Europe. They have performed in prestigious concert halls and festivals such as Teatro Colón (Argentina), Buenos Aires Jazz Festival (Argentina), Lincoln Center (US), Carnegie Hall (US), Marlboro Music Festival (US), Palais Montcalm (Canada), Rencontres Musicales Internationales des Graves (France), Lucerne Festival (Switzerland), and Mozarteum Festival (Austria). In addition to their active performance career, they are recognized for developing concert series, seminars, and master classes.\nOscuro Quintet has performed in prestigious venues such as Kimmel Center, Philadelphia Art Museum, Barnes Foundation, Tango Factory NY, and Embassy of Argentina in Washington, DC. In 2011, Oscuro Quintet released its album Music for Tango Ensemble on Big Round Records and an Emmy Award winning music and performance TV program On Tour featured the quintet in its half-hour show. In 2015, Oscuro Quintet was invited to perform for over one million people celebrating the World Meeting of Families and the historic visit of Pope Francis in Philadelphia. Recently, the quintet was artist-in-residence at Lebanon Valley College and performed at Jeonju International Sori Festival during their tour in Korea.</span></p>',
+		$content_control_path: '/brick/enduro_quill/quill_control'
 	},
 	something: {
 		image: '/remote/direct_uploads/1530501017_ab.jpg',
 		$image_type: 'image'
-	},
+	}
 })
